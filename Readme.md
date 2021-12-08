b8b0712210c175ac2afa4f4cd255c2bab535c8fd
José <jose.paumard@gmail.com>
2021-12-08 18:32:14 +0100

    First commit with everything.

---
+++ Readme.md
@@ -0,0 +1,46 @@
+## JEP Café episode 7: String Concatenation
+
+Here is the code used in [JEP Café 4](https://youtu.be/fZpRTH4J_3Q), where you will find the class I used to get the benchmarks. 
+
+This project is a Maven project, so you need a Maven installation to run it. 
+
+### Running the benchmark using Java 17
+
+To run the benchmark: 
+1. change directory to the root directory of this project. 
+2. make sure that you are using Java 17. You can check that by typing the following command in a console:
+```shell
+> java -version
+```
+3. Use maven to clean and package this project:
+```shell
+> mvn clean package
+```
+4. Then type in the following command:
+```shell
+> java -jar target/benchmarks.jar
+```
+
+Running the whole benchmark takes approximately 5mn on my desktop PC. The results you will get will be different than the one I have shown in the video. 
+
+### Running the benchmark using Java 8
+
+To run the benchmark using Java 8, you need two things:
+1. edit the java version in the `pom.xml` file of the project, from `17` to `8`. 
+2. make sure that you are using Java 8 with the command:
+```shell
+> java -version
+```
+You can then follow the previous instructions: cleaning and packaging your project, as well as running it are the same.  
+
+### Warning
+
+Let me copy / paste the warning JMH displays with every result it gives you: 
+
+> REMEMBER: The numbers below are just data. To gain reusable insights, you need to follow up on
+why the numbers are the way they are. Use profilers (see -prof, -lprof), design factorial
+experiments, perform baseline and negative tests that provide experimental control, make sure
+the benchmarking environment is safe on JVM/OS/HW level, ask for reviews from the domain experts.
+Do not assume the numbers tell you what you want them to tell.
+ 
+Diminishing the error can usually be done by increasing the number of iterations in the measurements and the warmup annotation on the `StringConcat` class. Your mileage may vary. 
---
+++ src/main/java/org/paumard/jepcafe/StringConcat.java
@@ -0,0 +1,122 @@
+package org.paumard.jepcafe;
+
+import org.openjdk.jmh.annotations.*;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.concurrent.TimeUnit;
+
+
+@Warmup(iterations = 20, time = 100, timeUnit = TimeUnit.MILLISECONDS)
+@Measurement(iterations = 30, time = 100, timeUnit = TimeUnit.MILLISECONDS)
+@Fork(value = 3)
+@BenchmarkMode(Mode.AverageTime)
+@OutputTimeUnit(TimeUnit.NANOSECONDS)
+@State(Scope.Benchmark)
+public class StringConcat {
+
+    @Param({"10"})
+    private int size;
+
+    @Param({"1000"})
+    private int converted;
+
+    private LocalDate date = LocalDate.of(2021, Month.SEPTEMBER, 17);
+
+    private String[] strings;
+
+    @Setup
+    public void init() {
+        strings = new String[size];
+        for (int index = 100000; index < 100000 + size; index++) {
+            strings[index - 100000] = "" + index;
+        }
+    }
+
+    @Benchmark
+    public String concat2() {
+        return strings[0] + strings[1];
+    }
+
+    @Benchmark
+    public String concat4() {
+        return strings[0] + strings[1] + strings[2] + strings[3];
+    }
+
+    @Benchmark
+    public String stringBuilder2() {
+        StringBuilder sb = new StringBuilder(1024);
+        sb.append(strings[0]).append(strings[1]);
+        return sb.toString();
+    }
+
+    @Benchmark
+    public String stringBuilder4() {
+        StringBuilder sb = new StringBuilder(1024);
+        sb.append(strings[0]).append(strings[1]).append(strings[2]).append(strings[3]);
+        return sb.toString();
+    }
+
+    @Benchmark
+    public String concat() {
+        String result = "";
+        for (String s : strings) {
+            result += s;
+        }
+        return result;
+    }
+
+    @Benchmark
+    public String append() {
+        StringBuilder sb = new StringBuilder();
+        for (String s : strings) {
+            sb.append(s);
+        }
+        return sb.toString();
+    }
+
+    @Benchmark
+    public String to_string() {
+        return Integer.toString(converted);
+    }
+
+    @Benchmark
+    public String string_int() {
+        return "" + converted;
+    }
+
+    @Benchmark
+    public String string_integer() {
+        return concatInteger(size);
+    }
+
+    private String concatInteger(Integer o) {
+        return "" + o;
+    }
+
+    @Benchmark
+    public String int_format() {
+        return String.format("%d %d %d %d %d", 123, 234, 345, 456, 567);
+    }
+
+    @Benchmark
+    public String date_format_1() {
+        return String.format("%tY-%tm-%td",
+                date, date, date);
+    }
+
+    @Benchmark
+    public String double_format_5() {
+        return String.format("%+7.4f %+7.4f %+7.4f %+7.4f %+7.4f", Math.PI, Math.PI, Math.PI, Math.PI, Math.PI);
+    }
+
+    @Benchmark
+    public String simple_double_format_5() {
+        return String.format("%f %f %f %f %f", Math.PI, Math.PI, Math.PI, Math.PI, Math.PI);
+    }
+
+    @Benchmark
+    public String string_format_5() {
+        return String.format("%s %s %s %s %s", "one", "two", "three", "four", "five");
+    }
+}
